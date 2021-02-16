9cf3d8115df9464722ca486b44980e8eeefce3a6
quintenvg1 <quintenvg1@gmail.com>
2021-02-16 09:03:45 +0100

    added arduino code starter project

---
+++ arduino_code/serial_connection/src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+int a;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()){
+    a = Serial.read(); //trying to read an integer sent from python (1 open gate, 2 close gate)
+    if(a == 1){
+
+    }
+  }
+
+}
