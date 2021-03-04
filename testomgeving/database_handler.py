d8ab7b1cb3a43f9d25dba3c101b7b8b825a0b644
quintenvg1 <quintenvg1@gmail.com>
2021-03-04 09:58:09 +0100

    handler updated

--- testomgeving/database_handler.py
+++ testomgeving/database_handler.py
@@ -26,11 +26,26 @@ class Databasecontroller:
     
     def createuser(naam, paswoord, plaat):
         cursor = mydb.cursor()
-        if(naam != ""):
+        if(naam != "" and paswoord != "" and plaat != ""): #check if there are values in the function call
             query = "insert into user (naam, paswoord, nummerplaat) values(" +"'"+ naam +"'" + "," +"'"+ paswoord +"'"+","+"'" + plaat +"'"+ ")"
             print(query)
             #query = "insert into user (naam, paswoord, nummerplaat) values ('John Cena', 'WrathOfTheCenation', '1-121-ABB')"
             cursor.execute(query)
             mydb.commit()
             # backend works
+    #end-createuser
+    
+    def deleteuser(naam, paswoord):
+        query = "delete from user where user.naam = " +"'" +naam +"'" +" AND user.paswoord = "+"'"+paswoord+"';"
+        cursor = mydb.cursor()
+        print(query)
+        cursor.execute(query)
+        mydb.commit()
+    #end-deleteuser
+    
+    def updateuser(naam, paswoord, nieuwenaam ,nieuwpaswoord, nieuweplaat):
+        #te moeilijk at the time of writing om al te implementeren
+        pass
+    #end-updateuser
+        
 
