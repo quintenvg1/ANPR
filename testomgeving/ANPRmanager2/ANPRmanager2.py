769891e9ab1a256b446aeea11fc0db2d3f2ef277
quintenvg1 <quintenvg1@gmail.com>
2021-03-10 09:25:43 +0100

    beginsels nieuwe gui

--- testomgeving/ANPRmanager2/ANPRmanager2.py
+++ testomgeving/ANPRmanager2/ANPRmanager2.py
@@ -1,7 +1,16 @@
 import wx
 import _thread as thread
 import time
+import datetime
 
+nummerplaat = ""
+naam = ""
+startdatum = ""
+einddatum = ""
+currentDate = ""
+pincode = 0000
+x = 100
+y = 50
 def sleep(duration): # too lazy to type out time.sleep every time
     time.sleep(duration)
 #end-sleep
@@ -10,10 +19,22 @@ class window1(wx.Frame):
             super(window1, self).__init__(parent=None, title='ANPR account manager', size=(720,480))
             panel = wx.Panel(self)
             
-            button1 = wx.Button(panel, label="bruh button")
-            button1.Bind(wx.EVT_BUTTON, self.button1)
+            #submit button
+            submit = wx.Button(panel, label="voeg toe", pos=(2*x, 3*y))
+            submit.Bind(wx.EVT_BUTTON, self.voegtoe)
+            submit.SetBackgroundColour(wx.Colour(255,165,0))
             
-        def button1(self, event):
+            #input fields
+            plate = wx.TextCtrl(panel, )
+            
+
+        def voegtoe(self, event):
+            global nummerplaat
+            global naam
+            global startdatum
+            global einddatum
+            global pincode
+            
             print("bruh")
         #end-button1
     
