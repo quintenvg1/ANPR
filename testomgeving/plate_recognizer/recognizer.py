94d6341aff7252cdadde0c15b65fb63c8843a4c5
quintenvg1 <quintenvg1@gmail.com>
2021-03-16 16:56:31 +0100

    changes to kernel and general operation of the script

--- testomgeving/plate_recognizer/recognizer.py
+++ testomgeving/plate_recognizer/recognizer.py
@@ -20,11 +20,17 @@ mydb = mysql.connect(
     )
 #should've created a connection
 
+try:
+    arduino = ser.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=0.1)
+except:
+    print("the arduino is not connected, or has to be programmed first")
+
 
 videoCaptureObject = cv2.VideoCapture(0)
 images = []
 res = "" # license plate
-arduino = ser.Serial("/dev/ttyUSB0", baudrate=9600, timeout=0.2) # initialize serial communication
+plaat = ""
+
 
 filterSize =(75, 75) 
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, filterSize)
@@ -33,7 +39,9 @@ def take10pictures():
     for i in range(10):
         ret,frame = videoCaptureObject.read()
         cv2.imwrite("pictures/picture" +str(i)+ ".jpg", frame)
+    #endfor
     videoCaptureObject.release()
+#end-take10pictures
 
 def recognize():
     filepath = "pictures/"
@@ -61,8 +69,8 @@ def recognize():
         # Applying the Black-Hat operation 
         tophat_img = cv2.morphologyEx(input_image, cv2.MORPH_BLACKHAT, kernel) 
         #cv2.imshow("original", input_image) 
-        cv2.imshow("tophat", tophat_img) 
-        cv2.waitKey()
+        #cv2.imshow("tophat", tophat_img) 
+        #cv2.waitKey()
         text = pytesseract.image_to_string(tophat_img)
         print("=================\n")
         print(image)
@@ -77,25 +85,20 @@ def recognize():
     #endfor
 #end-recognize
 
-try:
-    arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=0.1)
-except:
-    print("the arduino is not connected, or has to be programmed first")
-
 def opengate():
     global arduino
     x = "1"
     arduino.write(bytes(x.encode("utf8")))
+#end-opengate
 
 def closegate():
     global arduino
     x = "2"
     arduino.write(bytes(x.encode("utf-8")))
-
-
-
+#end-closegate
 
 def main():
+    global plaat
     #loop for this program
     print("entered program loop")
     #take a picture
@@ -104,6 +107,8 @@ def main():
     except:
         print("for some reason the pictures aren't coming through, check the connection")
     recognize() # check the images for license plates
+    plaat = res[0: (len(res) -2)]
+    print(plaat)
 
 main()
 
