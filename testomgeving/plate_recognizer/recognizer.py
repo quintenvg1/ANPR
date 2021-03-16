79f4ee488986cd989d09e8fa0a36e343f2cbca3f
quintenvg1 <quintenvg1@gmail.com>
2021-03-16 15:12:25 +0100

    recognition script progress

--- testomgeving/plate_recognizer/recognizer.py
+++ testomgeving/plate_recognizer/recognizer.py
@@ -1,6 +1,15 @@
 #database tools
 locations = ("agora", "gratiekapel", "middelheim") # lijst kan worden uitgebreid
 import mysql.connector as mysql
+#image processing tools
+import cv2
+import pytesseract
+import os
+from PIL import Image #use the pillow library
+import matplotlib as plt
+#serial connection tools
+import serial as ser
+
 
 mydb = mysql.connect(
     user = "quinten",
@@ -10,18 +19,14 @@ mydb = mysql.connect(
     )
 #should've created a connection
 
-#image processing tools
-import cv2
-import pytesseract
-import os
-from PIL import Image #use the pillow library
-import matplotlib as plt
-import serial as ser
 
 videoCaptureObject = cv2.VideoCapture(0)
 images = []
 res = ""
 arduino = ser.Serial("/dev/ttyUSB0", baudrate=9600, timeout=0.2) # initialize serial communication
+
+filterSize =(10, 10) 
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, filterSize)
 
 def take10pictures():
     for i in range(10):
@@ -30,51 +35,40 @@ def take10pictures():
         cv2.imwrite("pictures/picture" +str(i)+ ".jpg", frame)
     videoCaptureObject.release()
 
-def update_images():
+
+try:
+    arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=0.1)
+except:
+    print("the arduino is not connected, or has to be programmed first")
+
+
+def recognize():
     filepath = "pictures/"
     global images
     images = []
     print(os.listdir(filepath))
     for filename in os.listdir(filepath):
         if filename.endswith(".jpg") or filename.endswith(".png"):
-            images.append(filename)
+            print("pictures/"+filename)
+            images.append("pictures/"+filename)
             #put all the images in the array
         #endif
     #endfor
-
-def detectplates():
-    global images
-    for image in images:
-        pic = cv2.imread(image)
-        #cv2.imshow("picture", pic)
-        #cv2.waitKey()
-        
-        
-        
-#hardware control tools #change default usb port for arduino if it is on a different port
-
-import serial
-
-try:
-    arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=0.1)
-except:
-    print("the arduino is not connected, or has to be programmed first")
-def write(x):
-    arduino.write(bytes(x, 'utf-8'))
-
-def recognize():
-    global images # open images
+    
     global res # set the global result to the recognized text
     # Reading the image named 'input.jpg'
     f = open("lastscan.txt", "a")
     for image in images:
-        input_image = cv2.imread(image) 
+        print(image)
+        input_image = cv2.imread(image)
+        #cv2.imshow("input image", input_image)
+        #cv2.waitKey(0)
         input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
           
         # Applying the Black-Hat operation 
         tophat_img = cv2.morphologyEx(input_image, cv2.MORPH_BLACKHAT, kernel) 
-        #cv2.imshow("original", input_image) 
-        #cv2.imshow("tophat", tophat_img) 
+        cv2.imshow("original", input_image) 
+        cv2.imshow("tophat", tophat_img) 
         cv2.waitKey(500)
         text = pytesseract.image_to_string(tophat_img)
         print("=================\n")
@@ -107,8 +101,6 @@ def main():
         take10pictures()
     except:
         print("for some reason the pictures aren't coming through, check the connection")
-    update_images()
-    detectplates()
     recognize() # check the images for license plates
     print(images)
     #picture = cv2.imread("pictures/picture0.jpg")
